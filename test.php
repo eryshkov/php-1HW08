18f44f12351333fd8fa3ddcd43c04f196c3cb74c
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-18 18:15:29 +0300

    test.php is fixed

--- test.php
+++ test.php
@@ -3,7 +3,11 @@ require_once __DIR__ . '/classes/DB.php';
 
 $db = new DB(__DIR__ . '/config.php');
 
-$sql = 'SELECT * FROM persons';
+assert(true === $db->execute('SELECT * FROM persons WHERE id=1'));
+assert(false === $db->execute('SELECT * FROM persons WHERE idd=1'));
 
-$result = $db->execute($sql);
+$result = $db->query('SELECT * FROM persons WHERE id=:id', [':id'=>2]);
+assert(false === $db->query('SELECT * FROM persons WHERE igd=:id', [':id'=>1]));
+assert(true == $db->query('SELECT * FROM persons WHERE id=:id', [':id'=>1]));
+
 var_dump($result);
