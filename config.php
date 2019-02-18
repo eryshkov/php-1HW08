07ad1f86c9a55cd6c64f86fb5a4139e20f9ec31c
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-18 17:03:13 +0300

    db is added

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$result = [
+    'database' => 'mysql:php-1hw08.mac',
+    'dbPort' => '8889',
+    'dbName' => 'hw08',
+    'dbName' => 'hw08',
+
+];
+
+return $result;
--- index.php
+++ index.php
@@ -15,9 +15,7 @@
 <body>
 <p></p>
 <div class="container">
-    <a href="/01_guestbook/" class="btn btn-primary">01 GuestBook</a>
-    <a href="/02_gallery/" class="btn btn-primary">02 Gallery</a>
-    <a href="/03_news/news.php" class="btn btn-primary">03 News</a>
+    <a href="/test.php" class="btn btn-primary">Test</a>
 
 </div>
 </body>
