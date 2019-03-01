4887f46a62791c289e368670a9eb07cceb22515c
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-01 15:34:05 +0300

    article.php is fixed

--- article.php
+++ article.php
@@ -5,20 +5,28 @@ require_once __DIR__ . '/classes/View.php';
 require_once __DIR__ . '/classes/DB.php';
 
 
-if (isset($_GET['id'])) {
-    $articleNumber = $_GET['id'];
-
-    $db = new DB();
-    $data = $db->query('SELECT * FROM news WHERE id=:id', [':id' => $articleNumber]);
-
-    if (false !== $data) {
-        foreach ($data as $article) {
-            $article = new Article($article['id'], $article['author'], $article['title'], $article['text']);
-            $view = new View();
-            $view->assign('article', $article);
-            $view->display(__DIR__ . '/templates/newsArticle.php');
-            break;
-        }
-    }
+if (!isset($_GET['id'])) {
+    exit();
 }
+
+$articleNumber = $_GET['id'];
+
+$db = new DB();
+$data = $db->query('SELECT * FROM news WHERE id=:id', [':id' => $articleNumber]);
+if (false === $data) {
+    exit();
+}
+
+$article = reset($data);
+if (false === $article) {
+    exit();
+}
+
+$article = new Article($article['id'], $article['author'], $article['title'], $article['text']);
+$view = new View();
+$view->assign('article', $article);
+$view->display(__DIR__ . '/templates/newsArticle.php');
+
+
+
 
